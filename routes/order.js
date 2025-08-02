bfb45b2f874629dd9b356f37cd0b9239982c5b30
IbraheemRehan <ibrahimrehan1230@@gmail.com>
2025-08-01 18:52:46 -0700

    Testing

--- routes/order.js
+++ routes/order.js
@@ -3,17 +3,7 @@ const router = express.Router();
 const Order = require('../models/Order');
 const nodemailer = require('nodemailer');
 
-router.post("/", async (req, res) => {
-  try {
-    console.log("Received Order Payload:", req.body); 
-
-    // ... your logic
-  } catch (error) {
-    console.error("Error placing order:", error); 
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-const BASE_URL = process.env.BASE_URL || 'https://www.roycethreads.com';
+const BASE_URL = process.env.BASE_URL || 'https://royce-client.vercel.app';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
