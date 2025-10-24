eed9800dc52bb6032980512ec1a6453e7818f31c
IbraheemRehan <ibrahimrehan1230@@gmail.com>
2025-10-24 09:31:53 -0700

    Updated controller and  rroutes

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,21 @@ const fs = require('fs');
 const path = require('path');
 
 
+//  Get single product by ID
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.json(product);
+  } catch (err) {
+    console.error("Error fetching product:", err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+
 // Add new product
 const addProduct = async (req, res) => {
   try {
